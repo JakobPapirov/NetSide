dd394a8c2b7601ecc6754b4f2b4a59cc551e67d3
Jakob Papirov <JakobPapirov@users.noreply.github.com>
2021-06-15 08:45:59 +0200

    Stable

--- helper/showGameMenu.py
+++ helper/showGameMenu.py
@@ -41,6 +41,7 @@ def showGameMenu(char, menu, eventCheck, alive):
     print("==== Game options ====")
     print("")
 
+    # Add try/catch block to simulate player entering wrong input
     choiceUsr = input("What do you want to do [input number] and press enter: ")
 
     # Bug; character => error
@@ -50,8 +51,8 @@ def showGameMenu(char, menu, eventCheck, alive):
         choiceUsr = int(choiceUsr)
 
     char = choiceUserMenu(char, choiceUsr, menu, eventCheck, alive)
-    print("showGameMenu")
-    print(char)
+    # choiceUsr :: int
+    # menu :: list
     return char
 
 
@@ -69,28 +70,16 @@ def choiceUserMenu(char, choiceUsr, menu, eventCheck, alive):
         "Exit the game": netSideExit,
         "Wrong input": wrongInput
     }
-    # choiceUsr :: int
-    # menu :: list
-    # Add try/catch block to simulate player entering wrong input
-    playerChoiceInterpreted = menu[choiceUsr - 1] # Returns a str => used as key choiceUserOptions
-    # choiceUsr can only be 1, 2, 3 on the default pass-through
-    # => menu[1, 2, 3] => playerChoiceInterpreted = "Explore yourself",
-    #                                                   "Check your stats",
-    #                                                   "Exit the game",
+
+    playerChoiceInterpreted = menu[choiceUsr - 1] # Returns a str
+    # Used as key in choiceUserOptions
 
     # might need to rename var
     # keyListUserOptions :: list
     keyListUserOptions = list(choiceUserOptions.keys())
-    print(keyListUserOptions)
-
-    #listNums = range(len(keyListUserOptions))
-    #listNums = list( range( len(keyListUserOptions) ))
 
     # Add alive /Overlord\
     char = choiceUserOptions[playerChoiceInterpreted](char, alive)
-
-    print("choiceUserMenu post")
-    print("Status: ", alive)
 
     #return char, eventCheck, alive
     return char
